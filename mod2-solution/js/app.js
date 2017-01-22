47a427c606483fc51eda85c227b88896dc01f674
Prajwal Bati <prajwalbati@gmail.com>
2017-01-22 23:08:49 +0545

    Angular module 2 assignment complete

--- mod2-solution/js/app.js
+++ mod2-solution/js/app.js
@@ -9,36 +9,58 @@ angular.module('ShoppingListCheckOff', [])
 ToBuyController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyController(ShoppingListCheckOffService) {
   var buyItem = this;
+  buyItem.isEverythingBought = false;
 
-  buyItem.items = [{
-      "name" : "cookies",
-      "quantity" : 5
-    },{
-      "name" : "dairy milk",
-      "quantity" : 3
-    },{
-      "name" : "noodles",
-      "quantity" : 10
-    },{
-      "name" : "bread",
-      "quantity" : 4
-    },{
-      "name" : "butter",
-      "quantity" : 1
-    }];
+  buyItem.items = ShoppingListCheckOffService.getToBuyItems();
 
-  console.log(buyItem);
+  buyItem.removeItem = function(itemIndex) {
+    ShoppingListCheckOffService.removeItem(itemIndex);
+  };
+
 }
 
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtController(ShoppingListCheckOffService) {
   var boughtItem = this;
-  console.log(boughtItem);
+
+  boughtItem.items = ShoppingListCheckOffService.getBoughtItems();
 }
 
 function ShoppingListCheckOffService() {
   var checkOffService = this;
-  console.log(checkOffService);
+  var items = [
+      {
+        "name" : "cookies",
+        "quantity" : 5
+      }, {
+        "name" : "dairy milk",
+        "quantity" : 3
+      }, {
+        "name" : "noodles",
+        "quantity" : 10
+      }, {
+        "name" : "bread",
+        "quantity" : 4
+      }, {
+        "name" : "butter",
+        "quantity" : 1
+      }
+  ];
+
+  var boughtItems = [];
+
+  checkOffService.getToBuyItems = function() {
+    return items;
+  };
+
+  checkOffService.getBoughtItems = function() {
+      return boughtItems;
+  };
+
+  checkOffService.removeItem = function(itemIndex) {
+    boughtItems.push(items[itemIndex]);
+    items.splice(itemIndex, 1);
+  };
 }
 
 })();
