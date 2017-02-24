59d94bd9b909618b9d6a533916c8822ea65ef97f
Prajwal Bati <prajwalbati@gmail.com>
2017-02-25 00:59:44 +0545

    module 3 solution continue

--- mod3-solution/js/app.js
+++ mod3-solution/js/app.js
@@ -3,32 +3,45 @@
 
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
-.service('MenuSearchService', MenuSearchService);
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");;
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var narrow = this;
+  narrow.searchTerm = '';
+  narrow.foundItems = [];
 
-  MenuSearchService.getMatchedMenuItems();
+  narrow.getMatchedItems = function(searchTerm) {
+    if (searchTerm)
+      MenuSearchService.getMatchedMenuItems(searchTerm).then(function(data) {
+        narrow.foundItems = data;
+        console.log(narrow.foundItems);
+      });
+  };
+
 
 
 }
 
-MenuSearchService.$inject = ['$http']
-function MenuSearchService($http) {
+MenuSearchService.$inject = ['$http', 'ApiBasePath']
+function MenuSearchService($http, ApiBasePath) {
   var menuSearch = this;
 
-  menuSearch.getMatchedMenuItems = function() {
+  menuSearch.getMatchedMenuItems = function(searchTerm) {
+    var foundItems = [];
     return $http({
       method: "GET",
-      url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+      url: (ApiBasePath + "/menu_items.json")
     }).then(function (result) {
-        console.log(result);
-        // process result and only keep items that match
-        // var foundItems...
-
-        // return processed items
-        // return foundItems;
+      var menuItems = result.data.menu_items;
+      for (var i=0; i<menuItems.length; i++) {
+        var description = menuItems[i].description;
+        if (description.indexOf(searchTerm)!==-1) {
+          foundItems.push(menuItems[i]);
+        }
+      }
+      return foundItems;
     });
   };
 }
