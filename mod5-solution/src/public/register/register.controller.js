6801fc8e23608249ea1d7a76fcd228fd881b1db7
Prajwal Bati <prajwalbati@gmail.com>
2017-11-17 00:11:15 +0545

    Module 5 assignment

---
+++ mod5-solution/src/public/register/register.controller.js
@@ -0,0 +1,13 @@
+(function() {
+'use strict';
+
+angular.module("public")
+.controller('RegisterController', RegisterController);
+
+RegisterController.$inject = [];
+function RegisterController() {
+console.log("this is register controller");
+}
+
+
+})();
